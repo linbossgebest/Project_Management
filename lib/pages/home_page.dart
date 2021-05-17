abedb6511d2ccf63d2b7278013eab7a5c128a46a
linbossgebest <809621397@qq.com>
2021-05-17 10:12:37 +0800

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     //测试数据
-    var swiperImagelist = new List();
+    var swiperImagelist = [];
     swiperImagelist.add("lib/images/swip1.jpg");
     swiperImagelist.add("lib/images/swip2.jpg");
     swiperImagelist.add("lib/images/swip3.jpg");
